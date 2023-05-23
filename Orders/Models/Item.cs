d1fd30540d29e6450300395d9d220651b6f48fbc
Hans Emmanuel E. Duran <hansemmanuel.duran@cit.edu>
2023-05-23 12:35:53 +0800

    Add Item model

---
+++ Orders/Models/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace Orders.Models
+{
+	public class Item
+	{
+		public int Id { get; set; }
+		public int Quantity { get; set; }
+		public string Name { get; set; } = string.Empty;
+		public float Price { get; set; }
+	}
+}
