e56c3e150eab5e0bc6462e30d01613bd087a9128
Hans Emmanuel E. Duran <hansemmanuel.duran@cit.edu>
2023-05-23 13:45:37 +0800

    Change parameter type

--- Orders/Repositories/IRepository.cs
+++ Orders/Repositories/IRepository.cs
@@ -3,7 +3,7 @@
 	public interface IRepository<T>
 	{
 		public void Add(T entity);
-		void Remove(int id);
+		void Remove(T entity);
 		public IEnumerable<T> GetAll();
 		public T GetById(int id);
 		public void Update(T entity, T updatedEntity);
