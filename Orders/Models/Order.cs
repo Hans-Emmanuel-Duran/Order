97af33b8a05286e6c34bb4857bf90ea0c0854829
Hans Emmanuel E. Duran <hansemmanuel.duran@cit.edu>
2023-05-23 12:41:45 +0800

    Add item references

--- Orders/Models/Order.cs
+++ Orders/Models/Order.cs
@@ -1,9 +1,15 @@
 ﻿namespace Orders.Models
 {
-	public class Models
+	public class Order
 	{
 		public int Id { get; set; }
 		public DateTime OrderDate { get; set; }
 		public bool IsProcessed { get; set; }
+		
+		/// One-To-Many
+		/// <summary>
+		/// Reference to items ordered
+		/// </summary>
+		public ICollection<Item> Items { get; set; }
 	}
 }
