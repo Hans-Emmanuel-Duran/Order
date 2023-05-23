ffe0d3739c34585ed47dd5940919ce70accad177
Hans Emmanuel E. Duran <hansemmanuel.duran@cit.edu>
2023-05-23 13:37:55 +0800

    Create IRepository.cs

---
+++ Orders/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Orders.Repositories
+{
+	public interface IRepository<T>
+	{
+		public IEnumerable<T> GetAll();
+		public T GetById(int id);
+		public void Add(T entity);
+		public void Update(T entity, T updatedEntity);
+		void Delete(int id);
+		
+	}
+}
